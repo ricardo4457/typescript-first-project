01bf6238848ffc4e97a91fe50fb0868164ee765b
ricardo4457 <ricardo.a.vieira123@gmail.com>
2023-11-03 16:34:07 +0000

    Function types

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('testing');
+let boas;
+boas = () => { console.log('testing'); };
+console.log(boas());
+const add = (n1, n2, n3 = "nada") => { console.log(n3); };
+const reduce = (n1, n2, n3) => { console.log(n3); return n1 - n2; };
+console.log(add(12, 34, "arroz"));
+console.log(reduce(12, 34));
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,13 @@
-console.log('testing')
+let boas: Function;
+
+boas = () => { console.log('testing') };
+
+console.log(boas());
+
+const add = (n1: number, n2: number , n3: number | string = "nada" ) : void => {console.log(n3);}
+
+const reduce = (n1: number, n2: number , n3?: number ) : number => {console.log(n3); return n1 - n2 }
+
+console.log(add(12, 34 , "arroz"));
+
+console.log(reduce(12, 34));
