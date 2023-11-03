c7c3e41b0a8beeec023b4fdcc918cefa34b02b6a
ricardo4457 <ricardo.a.vieira123@gmail.com>
2023-11-03 16:44:40 +0000

    type aliases

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,13 +1,2 @@
-let boas: Function;
-
-boas = () => { console.log('testing') };
-
-console.log(boas());
-
-const add = (n1: number, n2: number , n3: number | string = "nada" ) : void => {console.log(n3);}
-
-const reduce = (n1: number, n2: number , n3?: number ) : number => {console.log(n3); return n1 - n2 }
-
-console.log(add(12, 34 , "arroz"));
-
-console.log(reduce(12, 34));
+type StringOrNum = string|number;
+type objOrName = { name: string, value: StringOrNum};
