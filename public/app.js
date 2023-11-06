6c1960f2151b42017e8ff42be5ae7fb444843b2f
ricardo4457 <ricardo.a.vieira123@gmail.com>
2023-11-06 14:47:51 +0000

    Enums exercise

--- public/app.js
+++ public/app.js
@@ -1,21 +1,20 @@
-// Interfaces with Classes
-import { Payment } from "./classes/Payment.js";
-import { Invoice } from "./classes/Invoice.js";
-import { ListTemplate } from "./classes/ListTemplate.js";
-//form
-const form = document.querySelector(".new-item-form");
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+const form = document.querySelector('.new-item-form');
+console.log(form.children);
 // inputs
-const type = document.querySelector("#type");
-const tofrom = document.querySelector("#tofrom");
-const details = document.querySelector("#details");
-const amount = document.querySelector("#amount");
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
 // list template instance
 const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
-    if (type.value === "invoice") {
+    if (type.value === 'invoice') {
         doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
     }
     else {
@@ -23,3 +22,24 @@ form.addEventListener("submit", (e) => {
     }
     list.render(doc, type.value, 'end');
 });
+// ENUMS
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+})(ResourceType || (ResourceType = {}));
+;
+const docOne = {
+    uid: 1,
+    resourceType: ResourceType.BOOK,
+    data: { title: 'name of the wind' }
+};
+const docTwo = {
+    uid: 10,
+    resourceType: ResourceType.DIRECTOR,
+    data: { title: 'name of the wind' }
+};
+console.log(docOne);
+console.log(docTwo);
