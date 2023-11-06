8f38e7a5b82cfb224accea415022a4fb4f605690
ricardo4457 <ricardo.a.vieira123@gmail.com>
2023-11-06 09:31:12 +0000

    Function Signatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,2 +1,27 @@
-type StringOrNum = string|number;
-type objOrName = { name: string, value: StringOrNum};
+// let boas: Function;
+
+// 1
+let boas:(a: string , b:string) => void;
+boas = (name:string,numID:string) =>{
+    console.log(`${name} says ${numID}`);
+};
+// 2
+
+let calc: ( a: number , b: number , c:string ) => number;
+
+
+calc: ( numbOne: number , numbTwo: number , action:string ) => { 
+    if(action = 'add') {
+    return numbOne + numbTwo;
+} else {
+    return numbOne - numbTwo;
+}
+}
+// 3
+
+let logDetails: ( obj: {name: string , age:number }) => void;
+
+logDetails =(person: {name: string , age: number})=> {
+
+    console.log(`${person.name} is ${person.age} years old`);   
+}
